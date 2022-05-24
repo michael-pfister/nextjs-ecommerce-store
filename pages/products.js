be0f10b500e52957cf0c2fb642a478aa1624825b
Michael Pfister <schoko.bombe44@gmail.com>
2022-05-24 18:11:18 +0200

    Simulate a Database and pass it as props

--- pages/products.js
+++ pages/products.js
@@ -1,5 +1,11 @@
 import { products } from './simulated_data/database';
 
-export default function Products() {
+export default function Products(props) {
   return <div>products</div>;
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: { products }, // will be passed to the page component as props
+  };
 }
--- pages/simulated_data/database.js
+++ pages/simulated_data/database.js
@@ -1,20 +1,62 @@
 /* https://www.gpumag.com/gpu-hierarchy/ */
-class Product {
+
+// not JSON serializable
+/* class Product {
   constructor(manufacturer, model, price) {
     this.manufacturer = manufacturer;
     this.model = model;
     this.price = price;
     this.inStock = true;
   }
-}
+} */
 
 export const products = [
-  new Product('AMD', 'RX 6950 XT', '1.308,36€'),
-  new Product('NVIDIA', 'RTX 3090 Ti', '1.914,96€'),
-  new Product('NVIDIA', 'RTX 3090', '1.914,96€'),
-  new Product('AMD', 'RX 6900 XT', '1.108,24€'),
-  new Product('NVIDIA', 'RTX 3080 Ti', '1.469,06€'),
-  new Product('NVIDIA', 'RTX 3080', '1.040,64€'),
-  new Product('AMD', 'RX 6800 XT', '1.108,24€'),
-  new Product('NVIDIA', 'Titan RTX', '3.792,00€'),
+  {
+    manufacturer: 'AMD',
+    model: 'RX 6950 XT',
+    price: '1.308,36€',
+    inStock: true,
+  },
+  {
+    manufacturer: 'NVIDIA',
+    model: 'RTX 3090 Ti',
+    price: '1.914,96€',
+    inStock: true,
+  },
+  {
+    manufacturer: 'NVIDIA',
+    model: 'RTX 3090',
+    price: '1.914,96€',
+    inStock: true,
+  },
+  {
+    manufacturer: 'AMD',
+    model: 'RX 6900 XT',
+    price: '1.108,24€',
+    inStock: true,
+  },
+  {
+    manufacturer: 'NVIDIA',
+    model: 'RTX 3080 Ti',
+    price: '1.469,06€',
+    inStock: true,
+  },
+  {
+    manufacturer: 'NVIDIA',
+    model: 'RTX 3080',
+    price: '1.040,64€',
+    inStock: true,
+  },
+  {
+    manufacturer: 'AMD',
+    model: 'RX 6800 XT',
+    price: '1.108,24€',
+    inStock: true,
+  },
+  {
+    manufacturer: 'NVIDIA',
+    model: 'Titan RTX',
+    price: '3.792,00€',
+    inStock: true,
+  },
 ];
